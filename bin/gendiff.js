c5219bcdd31d4eadc60637d13ada0d6cf3413463
Ангелина Головенко <lina-kursk@mail.ru>
2022-09-01 22:46:20 +0300

    add cli.js file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-const program = new Command();
 
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+import makeHelp from '../src/cli.js';
 
-program.parse();
+makeHelp();
---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import { Command } from 'commander';
+
+const makeHelp = () => {
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number');
+
+program.parse();
+}
+
+export default makeHelp;
