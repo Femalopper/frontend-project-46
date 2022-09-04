36d26d2ad398c742a1ed986b87aee909d0e456a8
Ангелина Головенко <lina-kursk@mail.ru>
2022-09-04 21:07:28 +0300

    add format option and arguments

--- src/cli.js
+++ src/cli.js
@@ -5,8 +5,9 @@ const makeHelp = () => {
 
   program
     .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number');
-
+    .version('0.0.1', '-V, --version', 'output the version number')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format');
   program.parse();
 };
 
