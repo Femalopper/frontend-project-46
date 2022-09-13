1cd3c4a29ba9524e53ff433294d197de30e4adcd
Ангелина Головенко <lina-kursk@mail.ru>
2022-09-13 16:42:34 +0300

    add stylish formatter

--- src/js/cli.js
+++ src/js/cli.js
@@ -7,13 +7,13 @@ const makeHelp = () => {
   program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
-    .parse();
-
-  const options = program.opts();
-  program
+    .option('-f, --format <type>', 'output format', 'stylish')
     .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, options.format)))
+    .action((filepath1, filepath2, format = program.opts().format) => console.log(genDiff(
+      filepath1,
+      filepath2,
+      format,
+    )))
     .parse();
 };
 
